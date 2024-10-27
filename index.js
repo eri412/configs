12c0a7930ce410733e9977ac852731d8119fb47a
eri412 <eri412@mail.ru>
2024-10-27 22:03:35 +0300

    index.js and package.json

---
+++ index.js
@@ -0,0 +1,3 @@
+export {default as configBase} from './eslint.config.base.js';
+export {default as configTypescript} from './eslint.config.typescript.js';
+export {default as configStylistic} from './eslint.config.stylistic.js';
